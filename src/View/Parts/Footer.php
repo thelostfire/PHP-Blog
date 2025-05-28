221fde81bb46b674ebcd7b51f49ee0dfc491e180
francois <francois.pegourie@live.fr>
2025-05-28 16:56:18 +0200

    ajout des pages header et footer

---
+++ src/View/Parts/Footer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\View\Parts;
+
+class Footer {
+   
+   public function render() {
+
+    ?>
+            </main>
+            <footer>
+                Placeholder
+            </footer>
+        </body>
+    </html>
+    <?php
+   }
+}
---
+++ src/View/Parts/Header.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\View\Parts;
+
+class Header {
+
+    public function render() {
+        ?> 
+        <!DOCTYPE html>
+        <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Nom d'une pipe</title>
+        </head>
+        <body>
+            <header>
+                Placeholder
+            </header>
+            <main>
+        <?php
+    }
+}
